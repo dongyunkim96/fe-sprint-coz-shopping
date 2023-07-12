ae3e07e03d1df368f8d6a17100f5208b3aedf1df
dongyunkim96 <dongyunkim96@gmail.com>
2023-07-11 23:49:01 -0400

    2nd draft header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Page/Header";
 
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-    </div>
+    </BrowserRouter>
   );
 }
 
